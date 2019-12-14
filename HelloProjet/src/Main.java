a572665c0b02c73f71b1e43c3722590e24359407
bigTeacherYan <635713139@qq.com>
2019-12-14 16:48:53 +0800

    第er次提交

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,5 +5,7 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+        String str ="45667867";
+        System.out.println("修改了");
     }
 }
