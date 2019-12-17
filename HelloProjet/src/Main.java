b44ad1c003fcc581fb77589be2d67737b52a0e9b
MoreTenYears <53559152+MoreTenYears@users.noreply.github.com>
2019-12-17 22:42:27 +0800

    Update Main.java
    别人修改

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-
+        System.out.println("别人改");
         System.out.println("修改");
     }
 }
